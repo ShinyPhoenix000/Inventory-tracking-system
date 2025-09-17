1ee825a6410844bdc6b8afb2518c528e6849c689
Kona Shiny Phoenix <konashiny475@gmail.com>
2025-09-18 00:49:11 +0530

    Delete all files from main branch

--- ui/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className, ...props }) => (
-  <input className={`border rounded px-3 py-2 w-full ${className ?? ''}`} {...props} />
-);
-
-export default Input;
-export { Input };
